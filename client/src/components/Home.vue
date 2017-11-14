1d98add95f56a41ee45a93157fc88d6b3d462a84
Salvatore Dziekan <sdziekan@gmail.com>
2017-11-14 13:57:48 -0500

    formatting table template

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -5,30 +5,23 @@
       <v-spacer></v-spacer>
       <v-text-field append-icon="search" label="Search" single-line hide-details v-model="search"></v-text-field>
     </v-card-title>
-    <v-data-table v-bind:headers="headers" v-bind:items="items" v-bind:search="search">
+    <v-data-table hide-actions v-bind:headers="headers" v-bind:items="items" v-bind:search="search">
       <template slot="items" slot-scope="props">
-        <td>
-          <v-edit-dialog lazy> {{ props.item.name }}
-            <v-text-field slot="input" label="Edit" v-model="props.item.name" single-line counter :rules="[max25chars]"></v-text-field>
-          </v-edit-dialog>
+        <td class="text-xs-center">
+          {{ props.item.name }}
         </td>
         <td class="text-xs-right">{{ props.item.calories }}</td>
-        <td class="text-xs-right">{{ props.item.fat }}</td>
+        <td class="text-xs-right">
+          <a v-bind:href="props.item.iron" target=”_blank”>{{ props.item.fat }}</a>
+
+        </td>
         <td class="text-xs-right">{{ props.item.carbs }}</td>
         <td class="text-xs-right">{{ props.item.protein }}</td>
         <td class="text-xs-right">{{ props.item.sodium }}</td>
         <td class="text-xs-right">{{ props.item.calcium }}</td>
-        <td class="text-xs-right">
-          <v-edit-dialog @open="tmp = props.item.iron" @save="props.item.iron = tmp || props.item.iron" large lazy>
-            <div>{{ props.item.iron }}</div>
-            <div slot="input" class="mt-3 title">Update Iron</div>
-            <v-text-field slot="input" label="Edit" v-model="tmp" single-line counter autofocus :rules="[max25chars]"></v-text-field>
-          </v-edit-dialog>
-        </td>
+
       </template>
-      <template slot="pageText" slot-scope="{ pageStart, pageStop }">
-        From {{ pageStart }} to {{ pageStop }}
-      </template>
+
     </v-data-table>
   </v-card>
 </template>
@@ -40,15 +33,12 @@ export default {
   data() {
     return {
       msg: 'Welcome to Your HomePage',
-      max25chars: (v) => v.length <= 25 || 'Input too long!',
       tmp: '',
       search: '',
-      pagination: {},
       headers: [
         {
           text: 'Dessert (100g serving)',
           align: 'left',
-          sortable: false,
           value: 'name'
         },
         { text: 'Calories', value: 'calories' },
@@ -56,8 +46,7 @@ export default {
         { text: 'Carbs (g)', value: 'carbs' },
         { text: 'Protein (g)', value: 'protein' },
         { text: 'Sodium (mg)', value: 'sodium' },
-        { text: 'Calcium (%)', value: 'calcium' },
-        { text: 'Iron (%)', value: 'iron' }
+        { text: 'Calcium (%)', value: 'calcium' }
       ],
       items: [
         {
@@ -65,11 +54,11 @@ export default {
           name: 'Frozen Yogurt',
           calories: 159,
           fat: 6.0,
-          carbs: 24,
+          carbs: [24, 36, 22, 55],
           protein: 4.0,
           sodium: 87,
           calcium: '14%',
-          iron: '1%'
+          iron: 'http://www.test.com'
         },
         {
           value: false,
@@ -80,7 +69,7 @@ export default {
           protein: 4.3,
           sodium: 129,
           calcium: '8%',
-          iron: '1%'
+          iron: 'http://www.test1.com'
         },
         {
           value: false,
@@ -91,7 +80,7 @@ export default {
           protein: 6.0,
           sodium: 337,
           calcium: '6%',
-          iron: '7%'
+          iron: 'http://www.test2.com'
         },
         {
           value: false,
@@ -102,7 +91,7 @@ export default {
           protein: 4.3,
           sodium: 413,
           calcium: '3%',
-          iron: '8%'
+          iron: 'http://www.test3.com'
         },
         {
           value: false,
@@ -113,7 +102,7 @@ export default {
           protein: 3.9,
           sodium: 327,
           calcium: '7%',
-          iron: '16%'
+          iron: 'http://www.test4.com'
         },
         {
           value: false,
@@ -124,7 +113,7 @@ export default {
           protein: 0.0,
           sodium: 50,
           calcium: '0%',
-          iron: '0%'
+          iron: 'http://www.test5.com'
         },
         {
           value: false,
@@ -135,7 +124,7 @@ export default {
           protein: 0,
           sodium: 38,
           calcium: '0%',
-          iron: '2%'
+          iron: 'http://www.test6.com'
         },
         {
           value: false,
@@ -146,7 +135,7 @@ export default {
           protein: 6.5,
           sodium: 562,
           calcium: '0%',
-          iron: '45%'
+          iron: 'http://www.test7.com'
         },
         {
           value: false,
@@ -157,7 +146,7 @@ export default {
           protein: 4.9,
           sodium: 326,
           calcium: '2%',
-          iron: '22%'
+          iron: 'http://www.test8.com'
         },
         {
           value: false,
@@ -168,12 +157,20 @@ export default {
           protein: 7,
           sodium: 54,
           calcium: '12%',
-          iron: '6%'
+          iron: 'http://www.test9.com'
         }
       ]
     }
+  },
+  computed: {
+    //function to place items within an array each on their own line
+    newLineWithinCell() {
+
+    }
   }
+
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
